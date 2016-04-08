6fbe63b4d7eebd992480d28b72d13dd5f2fd83e5
guoxuri <853523391@qq.com>
2016-04-08 18:37:14 +0800

    测试null集合

--- TestSolution/TestCollectionWhereNull/Program.cs
+++ TestSolution/TestCollectionWhereNull/Program.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestCollectionUpdate
+namespace TestCollectionWhereNull
 {
     class Program
     {
         static void Main(string[] args)
         {
             List<UserInfo> listUserInfo = new List<UserInfo>() { new UserInfo { ID = 1, Name = "A", Age = 20 }, new UserInfo { ID = 2, Name = "B", Age = 20 } };  //实例化集合
-            int index = listUserInfo.IndexOf(listUserInfo.Where(p => p.ID == 1).FirstOrDefault());
-            if (index > -1)
-            {
-                listUserInfo[index].Age += 1;
-            }
-            listUserInfo.ForEach(p => Console.WriteLine("ID:" + p.ID + " Name:" + p.Name + " Age:" + p.Age));
+            UserInfo user = listUserInfo.Where(p=>p.ID==2).FirstOrDefault();
+            Console.WriteLine("ID:" + user.ID + " Name:" + user.Name + " Age:" + user.Age);
             Console.ReadKey();
         }
     }
