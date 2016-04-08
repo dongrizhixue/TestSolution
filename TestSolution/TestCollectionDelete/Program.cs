1ce8ef0fba2648d53788467d4da923976064901f
guoxuri <853523391@qq.com>
2016-04-08 10:42:57 +0800

    集合删除测试 close #1

--- TestSolution/TestCollectionDelete/Program.cs
+++ TestSolution/TestCollectionDelete/Program.cs
@@ -10,6 +10,18 @@ namespace TestCollectionDelete
     {
         static void Main(string[] args)
         {
+            List<UserInfo> listUserInfo = new List<UserInfo>() { new UserInfo { ID = 1, Name = "A" }, new UserInfo { ID = 2, Name = "B" } };  //实例化集合
+            listUserInfo.Remove(listUserInfo.Where(p => p.ID == 1).FirstOrDefault());   //从集合中删除实体
+            foreach (UserInfo item in listUserInfo)
+            {
+                Console.WriteLine("ID:" + item.ID + " Name:" + item.Name);
+            }
+            Console.ReadKey();
         }
+    }
+    class UserInfo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
