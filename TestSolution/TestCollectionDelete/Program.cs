06e521b7d899e375f7db789d785d555bf610659b
guoxuri <853523391@qq.com>
2016-04-11 09:25:52 +0800

    测试用属性完全相同的实体，能否在集合中删除另一个实体。close #3

--- TestSolution/TestCollectionDelete/Program.cs
+++ TestSolution/TestCollectionDelete/Program.cs
@@ -10,6 +10,14 @@ namespace TestCollectionDelete
     {
         static void Main(string[] args)
         {
+            Delete2();
+        }
+
+        /// <summary>
+        /// 匿名对象的删除
+        /// </summary>
+        private static void Delete1()
+        {
             List<UserInfo> listUserInfo = new List<UserInfo>() { new UserInfo { ID = 1, Name = "A" }, new UserInfo { ID = 2, Name = "B" } };  //实例化集合
             listUserInfo.Remove(listUserInfo.Where(p => p.ID == 1).FirstOrDefault());   //从集合中删除实体
             foreach (UserInfo item in listUserInfo)
@@ -18,10 +26,26 @@ namespace TestCollectionDelete
             }
             Console.ReadKey();
         }
-    }
-    class UserInfo
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
+        /// <summary>
+        /// 实体对象的删除
+        /// </summary>
+        private static void Delete2()
+        {
+            List<UserInfo> listUserInfo = new List<UserInfo>();
+            UserInfo user = new UserInfo();   //实体1
+            user.ID = 1;
+            user.Name = "A";
+            listUserInfo.Add(user);
+            UserInfo user2 = new UserInfo();  //实体2
+            user2.ID = 2;
+            user2.Name = "B";
+            listUserInfo.Add(user2);
+            UserInfo user3 = new UserInfo();   //模仿实体1
+            user3.ID = 1;
+            user3.Name = "A";
+            listUserInfo.Remove(user3);   // 试图用实体3模仿的实体1来删除实体1
+            listUserInfo.ForEach(p => Console.WriteLine("ID:" + p.ID + " Name:" + p.Name));
+            Console.ReadKey();
+        }
     }
 }
---
+++ TestSolution/TestCollectionDelete/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCollectionDelete
+{
+    public class UserInfo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
