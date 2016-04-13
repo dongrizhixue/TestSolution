def3d880473e5411464632db2120e8d0e12bad5e
guoxuri <853523391@qq.com>
2016-04-13 11:13:19 +0800

    测试集合更新的可靠代码写法。close #4

--- TestSolution/TestCollectionUpdate/Program.cs
+++ TestSolution/TestCollectionUpdate/Program.cs
@@ -10,12 +10,28 @@ namespace TestCollectionUpdate
     {
         static void Main(string[] args)
         {
+            Update1();
+        }
+
+        /// <summary>
+        /// 采用找元素的方法更新集合
+        /// </summary>
+        private static void Update1()
+        {
             List<UserInfo> listUserInfo = new List<UserInfo>() { new UserInfo { ID = 1, Name = "A", Age = 20 }, new UserInfo { ID = 2, Name = "B", Age = 20 } };  //实例化集合
-            int index = listUserInfo.IndexOf(listUserInfo.Where(p => p.ID == 1).FirstOrDefault());
+            int index = listUserInfo.IndexOf(listUserInfo.Where(p => p.ID == 2).FirstOrDefault());
             if (index > -1)
             {
                 listUserInfo[index].Age += 1;
             }
+            listUserInfo.ForEach(p => Console.WriteLine("ID:" + p.ID + " Name:" + p.Name + " Age:" + p.Age));
+            Console.ReadKey();
+        }
+
+        private static void Update2()
+        {
+            List<UserInfo> listUserInfo = new List<UserInfo>() { new UserInfo { ID = 1, Name = "A", Age = 20 }, new UserInfo { ID = 2, Name = "B", Age = 20 } };  //实例化集合
+
             listUserInfo.ForEach(p => Console.WriteLine("ID:" + p.ID + " Name:" + p.Name + " Age:" + p.Age));
             Console.ReadKey();
         }
